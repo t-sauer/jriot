2d990ffe7c8539625e99f6e751613772e4b9e8cf
Thomas Oliver Sauer <thomas.sauer@uni-ulm.de>
2013-12-11 16:16:03 +0100

    Async calls
    - asynchronous calls should be possible now
    - changed summonerId from int to long to fit the Riot API

---
+++ src/jriot/main/ApiCaller.java
@@ -0,0 +1,51 @@
+package jriot.main;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiCaller {
+	
+	
+	public String request(String URL) {
+		
+		try {
+			String requestURL = URL;
+			
+			URL url = new URL(requestURL);
+			
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			
+			connection.setDoOutput(true);
+			connection.setRequestMethod("GET");
+			connection.setInstanceFollowRedirects(false); 
+			
+
+			InputStream is = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			String line;
+			StringBuffer response = new StringBuffer(); 
+			
+			while((line = rd.readLine()) != null) {
+				response.append(line);
+				response.append('\r');
+			}
+			
+			connection.disconnect();
+			
+			
+			return response.toString();
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+
+}
--- src/jriot/main/JRiot.java
+++ src/jriot/main/JRiot.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,10 +19,7 @@ import jriot.objects.*;
 public class JRiot {
 	
 	private String apiKey;
-	
-	private String response;
-	
-	private String ApiURL;
+	private String region;
 	
 	private String baseURL1 = "http://prod.api.pvp.net/api/lol/na/v1.1/";
 	private String baseURL2 = "http://prod.api.pvp.net/api/na/v2.1/";
@@ -29,173 +27,217 @@ public class JRiot {
 	
 	Gson gson = new Gson();
 	
-	private  void doRequest() {
+	
+	public JRiot(String key, String region) {
+		super();
 		
-		try {
-			String requestURL = ApiURL;
-			
-			URL url = new URL(requestURL);
-			
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			
-			connection.setDoOutput(true);
-			connection.setRequestMethod("GET");
-			connection.setInstanceFollowRedirects(false); 
-			
-
-			InputStream is = connection.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			String line;
-			StringBuffer response = new StringBuffer(); 
-			
-			while((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			
-			this.response = response.toString();
-			
-			
-			connection.disconnect();
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		this.setApiKey(key);
+		this.setRegion(region);
 	}
+	
+	public JRiot() {
+		super();
+	}
+	
+	/**
+	 * Sets the API key you want to use.
+	 * 
+	 * @param key Your API key.
+	 */
 	
 	public void setApiKey(String key) {
 		this.apiKey = key;
 	}
 	
+	/**
+	 * Sets the region where to get the data from.
+	 * 
+	 * @param region the region (EUNE, EUW, NA, BR, TR).
+	 */
 	public void setRegion(String region) {
 		
 		this.baseURL1 = "http://prod.api.pvp.net/api/lol/" + region + "/v1.1/";
-		baseURL2 = "http://prod.api.pvp.net/api/" + region + "/v2.1/";
+		this.baseURL2 = "http://prod.api.pvp.net/api/" + region + "/v2.1/";
+		
+		this.region = region;
 		
 	}
 	
+	/**
+	 * Retrieve all current champions.
+	 * 
+	 * @return List of all Champions.
+	 */
 	public ChampionList getChampions() {
-		this.ApiURL = this.baseURL1 + "v1.1/champion"+"?api_key="+apiKey;
-		doRequest();
-		
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "champion"+"?api_key="+apiKey);
 		ChampionList championList = gson.fromJson(response, ChampionList.class);
 		
 		return championList;
 	}
 	
+	/**
+	 * Retrieve all free champions.
+	 * 
+	 * @return List of all free Champions.
+	 */
 	public ChampionList getFreeChampions() {
-		this.ApiURL = this.baseURL1 + "champion"+"?freeToPlay=true&api_key="+apiKey;
-		doRequest();
-		
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "champion"+"?freeToPlay=true&api_key="+apiKey);
 		ChampionList championList = gson.fromJson(response, ChampionList.class);
 		
 		return championList;
 	}
 	
-	public RecentGames getRecentGames(int summonerId) {
-		
-		this.ApiURL = this.baseURL1 + "game/by-summoner/"+summonerId+"/recent"+"?api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get recently played games by a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return RecentGames objects which contains the last played games.
+	 */
+	public RecentGames getRecentGames(long summonerId) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "game/by-summoner/"+summonerId+"/recent"+"?api_key="+apiKey);
 		RecentGames recentGames = gson.fromJson(response, RecentGames.class);
 		
 		return recentGames;
 		
 	}
 	
-	public Map<String, League> getLeagues(int summonerId) {		
-		
-		this.ApiURL = this.baseURL2 + "league/by-summoner/"+summonerId+"?api_key="+apiKey;
-		doRequest();
+	/**
+	 * Get all leagues of a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return Returns a map of either the summonerId (in case of solo queue) 
+	 * 			or the teamId with the corresponding league.
+	 */
+	public Map<String, League> getLeagues(long summonerId) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL2 + "league/by-summoner/"+summonerId+"?api_key="+apiKey);
 		Map<String, League> leagues =  gson.fromJson(response, new TypeToken<Map<String, League>>(){}.getType());
 		
 		return leagues;
 		
 	}
 	
-	public PlayerStatsSummaryList getPlayerStatsSummaryList(int summonerId, int season) {
-		this.ApiURL = this.baseURL1 + "stats/by-summoner/"+summonerId+"/summary"+"?season=SEASON"+season+"&api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get all player stats of a given summoner and a given season. 
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @param season Specifies the season you want the stats about (currently 3 or 4)
+	 * @return PlayerStatsSummaryList that contains the stats
+	 */
+	public PlayerStatsSummaryList getPlayerStatsSummaryList(long summonerId, int season) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "stats/by-summoner/"+summonerId+"/summary"+"?season=SEASON"+season+"&api_key="+apiKey);
 		PlayerStatsSummaryList playerStatsSummaryList = gson.fromJson(response, PlayerStatsSummaryList.class);
 		
 		return playerStatsSummaryList;
 	}
 	
-	public RankedStats getRankedStats(int summonerId, int season) {
-		this.ApiURL = this.baseURL1 + "stats/by-summoner/"+summonerId+"/ranked"+"?season=SEASON"+season+"&api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get all ranked stats of a given summoner and a given season. 
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @param season Specifies the season you want the stats about (currently 3 or 4)
+	 * @return Ranked that contains the stats.
+	 */
+	public RankedStats getRankedStats(long summonerId, int season) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "stats/by-summoner/"+summonerId+"/ranked"+"?season=SEASON"+season+"&api_key="+apiKey);
 		RankedStats rankedStats = gson.fromJson(response, RankedStats.class);
 		
 		return rankedStats;
 	}
 	
-	public MasteryPages getMasteryPages(int summonerId) {
-		this.ApiURL = this.baseURL1 + "summoner/"+summonerId+"/masteries?api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get all Mastery Pages of a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return MasteryPages object that contains a list of all Mastery Pages
+	 */
+	public MasteryPages getMasteryPages(long summonerId) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "summoner/"+summonerId+"/masteries?api_key="+apiKey);
 		MasteryPages masteryPages = gson.fromJson(response, MasteryPages.class);
 		
 		return masteryPages;
 	}
 	
-	public RunePages getRunePages(int summonerId) {
-		this.ApiURL = this.baseURL1 + "summoner/"+summonerId+"/runes?api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get all Rune Pages of a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return RunePages object that contains a list of all Rune Pages
+	 */
+	public RunePages getRunePages(long summonerId) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL1 + "summoner/"+summonerId+"/runes?api_key="+apiKey);
 		RunePages runePages = gson.fromJson(response, RunePages.class);
 		
 		return runePages;
 	}
 	
-	public Summoner getSummoner(int summonerId) {
-		
+	/**
+	 * Get information about a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return Summoner object, containing information.
+	 */
+	public Summoner getSummoner(long summonerId) {
+		ApiCaller caller = new ApiCaller();
 		Gson gson = new GsonBuilder().setDateFormat("MM/dd/yyy HH:mm").create();
-		this.ApiURL = this.baseURL1 + "summoner/"+summonerId+"?api_key="+apiKey;
-		doRequest();
-		
+		String response = caller.request(this.baseURL1 + "summoner/"+summonerId+"?api_key="+apiKey);
 		Summoner summoner = gson.fromJson(response, Summoner.class);
 		
 		return summoner;
 	}
 	
+	/**
+	 * Get information about a given summoner.
+	 * 
+	 * @param summonerId Name of a summoner.
+	 * @return Summoner object, containing information.
+	 */
 	public Summoner getSummoner(String summonerName) {
-		
+		ApiCaller caller = new ApiCaller();
 		Gson gson = new GsonBuilder().setDateFormat("MM/dd/yyy HH:mm").create();
-		this.ApiURL = this.baseURL1 + "summoner/by-name/"+summonerName+"?api_key="+apiKey;
-		doRequest();
-		
+		String response = caller.request(this.baseURL1 + "summoner/by-name/"+summonerName+"?api_key="+apiKey);
 		Summoner summoner = gson.fromJson(response, Summoner.class);
 		
 		return summoner;
 	}
 	
-	public SummonerNameList getSummoners(List<Integer> summonerIds) {
-		
+	/**
+	 * Get information about more than one summoner at the same time.
+	 * 
+	 * @param summonerIds List of summonerIds.
+	 * @return SummonerNameList containing the information about the given summoners.
+	 */
+	public SummonerNameList getSummoners(List<Long> summonerIds) {
+		ApiCaller caller = new ApiCaller();
 		String ids = "";
-		for(int i : summonerIds) {
+		for(long i : summonerIds) {
 			ids = ids + i + ",";
 		}
-		this.ApiURL = this.baseURL1 + "summoner/"+ids+"/name?api_key="+apiKey;
-		doRequest();
-		
+		String response = caller.request(this.baseURL1 + "summoner/"+ids+"/name?api_key="+apiKey);
 		SummonerNameList summonerNameList = gson.fromJson(response, SummonerNameList.class);
 		
 		return summonerNameList;
 	}
 	
-	public List<Team> getTeams(int summonerId) {
-
-		this.ApiURL = this.baseURL2 + "team/by-summoner/"+summonerId+"?api_key="+apiKey;
-		doRequest();
-		
+	/**
+	 * Get all teams of a given summoner.
+	 * 
+	 * @param summonerId Id of a summoner.
+	 * @return A list containing Team objects.
+	 */
+	public List<Team> getTeams(long summonerId) {
+		ApiCaller caller = new ApiCaller();
+		String response = caller.request(this.baseURL2 + "team/by-summoner/"+summonerId+"?api_key="+apiKey);
 		List<Team> team = gson.fromJson(response, List.class);
 		
 		return team;
 	}
-	
 
 }
